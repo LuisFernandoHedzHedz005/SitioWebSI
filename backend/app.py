6bafa5841de0a228f3dcfc3a5957ed8d5d17e410
LuisFernandoHedzHedz005 <luisfdohernandez005@gmail.com>
2025-09-26 07:35:21 -0600

    Añadiendo validaciones de regiustro, archivo de correos temporales, permisos de CORS, modificaciones de visivilidad de datos sensibles y funciones extra para validaciones

--- backend/app.py
+++ backend/app.py
@@ -3,30 +3,87 @@ from flask_cors import CORS
 from pymongo import MongoClient
 import os, bcrypt, jwt
 from dotenv import load_dotenv
+import re
+import dns.resolver
 
 load_dotenv()
 app = Flask(__name__)
-CORS(app)
+origins = [ os.getenv("LOCAL_URL"), os.getenv("PROD_URL") ]
+CORS(app, resources={r"/api/*": {"origins": origins}})
+
 
 MONGO_URI = os.getenv("MONGO_URI")
 
 #Eliminacion de print de mongoURI
 print(f"--- Intentando conectar con URI: ---")
-JWT_SECRET = os.getenv("SECRET_KEY", "devsecret")
+
+#eliminando secret key por defecto en caso de no estar presente en el archivo .env
+JWT_SECRET = os.getenv("SECRET_KEY")
+if not JWT_SECRET:
+    raise ValueError("No se encontró la variable de entorno SECRET_KEY")
 
 client = MongoClient(MONGO_URI, tls=True)
 db = client.get_default_database()
 users = db.users
+
+blocklist = None
+
+def load_blocklist():
+    global blocklist
+    if blocklist is None:
+        try:
+            filepath = os.path.join(os.path.dirname(__file__), 'disposable_email_blocklist.conf')
+            with open(filepath, 'r', encoding='utf-8') as f:
+                blocklist = [line.strip() for line in f if line.strip()]
+                print("lista de dominios cargada con exito")
+        except FileNotFoundError:
+            print("Archivo de lista de bloqueo no encontrado. No se aplicarán restricciones de correo desechable.")
+            blocklist = []
+        except Exception as e:
+            print(f"Error al obtener la ruta del archivo: {e}")
+            blocklist = []
+            return
+        
+def is_disposable_email(email):
+    if blocklist is None:
+        load_blocklist()
+    if "@" not in email:
+        return False
+    
+    domain = email.split('@')[-1]
+    return domain in blocklist
+
+def validate_email_estructure(email):
+    exp_reg = r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
+    return re.match(exp_reg, email) is not None
 
 @app.route("/api/register", methods=["POST"])
 def register():
     data = request.get_json()
     email = data.get("email")
     password = data.get("password")
-    role = data.get("role", "user")
+    # En vez de recibir el rol de user desde el frontend,, asignarlo automaticamente para evitar escalacion de privilegios
+    # role = data.get("role", "user")
+    role = "user"
+
+     # Validar que el rol sea 'user' para evitar escalación de privilegios
+    if role != "user":
+        return jsonify({"error": "rol inválido"}), 400
+    
+    if not email or not password:
+        return jsonify({"error": "email y contraseña son requeridos"}), 400
+    
+    if not validate_email_estructure(email):
+        return jsonify({"error": "estructura de email inválida"}), 400
+    
+    if is_disposable_email(email):
+        return jsonify({"error": "no se permiten correos desechables"}), 400
 
     if users.find_one({"email": email}):
         return jsonify({"error": "usuario ya existe"}), 400
+    
+    if len(password) < 6:
+        return jsonify({"error": "la contraseña debe tener al menos 6 caracteres"}), 400
 
     pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     users.insert_one({"email": email, "password": pw_hash, "role": role})
